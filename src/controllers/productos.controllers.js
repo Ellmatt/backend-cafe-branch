703760b89bf38c61d1d9d48606d2b99b5851c40c
matias <pagosptcmatias@gmail.com>
2022-10-25 18:30:18 -0300

    agregamos la logica para crear 8in producto y obtener un arreglo de todos los productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,14 +1,35 @@
+import Producto from "../models/producto";
 
-
-
-export const listarProductos = (req, res) => {
-    res.send("aqui tengo que retornar un arreglo de productos");
+export const listarProductos = async (req, res) => {
+  try {
+    // guardar ese producto en la bd
+    const listaProductos = await Producto.find();
+    // find siempre devuelve un arreglo 
+    // responder al usuario que todo salio bien
+    res.status(200).json(listaProductos);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+        mensaje: 'Error al intentar buscar los prodictos'
+    });
   }
-  export const crearProducto = (req, res) => {
+};
+
+export const crearProducto = async (req, res) => {
+  try {
     // extraer del body los dato
-    console.log(req.body)
-    // agregar la validacion correspondiente 
-    
-    // guardar ese producto ne la bd
-    res.send("aqui devolvemos un producto");
+    console.log(req.body);
+    // agregar la validacion correspondiente
+
+    const productoNuevo = new Producto(req.body);
+    // guardar ese producto en la bd
+    await productoNuevo.save();
+    // responder al usuario que todo salio bien
+    res.status(201).json({ mensaje: "El producto fue correctamente creado" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "Error al intentar agregar un producto",
+    });
   }
+};
