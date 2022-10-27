5cf1f685996eb4d1131208507a808e0e34c49c70
matias <pagosptcmatias@gmail.com>
2022-10-27 18:41:27 -0300

    agregamos validaciones

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,22 +1,32 @@
+import { validationResult } from "express-validator";
 import Producto from "../models/producto";
 
 export const listarProductos = async (req, res) => {
   try {
     // guardar ese producto en la bd
     const listaProductos = await Producto.find();
-    // find siempre devuelve un arreglo 
+    // find siempre devuelve un arreglo
     // responder al usuario que todo salio bien
     res.status(200).json(listaProductos);
   } catch (error) {
     console.log(error);
     res.status(404).json({
-        mensaje: 'Error al intentar buscar los prodictos'
+      mensaje: "Error al intentar buscar los prodictos",
     });
   }
 };
 
 export const crearProducto = async (req, res) => {
   try {
+    // manejar lo errores de express validator
+    const errores = validationResult(req);
+    // errores.isEmpty() retorna true cuendo no hay errores y false cuando hay errores
+    // pregunto si hay errores
+    if (!errores.isEmpty()) {
+      return res.status(400).json({
+        errores: errores.array(),
+      });
+    }
     // extraer del body los dato
     console.log(req.body);
     // agregar la validacion correspondiente
@@ -34,48 +44,48 @@ export const crearProducto = async (req, res) => {
   }
 };
 
-export const obtenerProducto = async(req, res)=>{
-try{
-// obtener el parametro
-console.log(req.params.id)
-// peditle a la bd buscar el documento que concide con el id del parametro
-const productoBuscado = await Producto.findById(req.params.id)
-// responder con el producto encontrado
-res.status(200).json(productoBuscado)
-}catch(error){
-  console.log(error)
-  res.status(404).json({
-    mensaje:'Error no se pudo encontrar el producto solicitado'
-  })
-}
-}
-export const editarProducto = async(req, res)=>{
-try{
-// buscar el producto por el id, luego modificar los datos con el body 
-await Producto.findByIdAndUpdate(req.params.id, req.body)
-// responder al frontend
-res.status(200).json({
-  mensaje:'El producto fue editado correctamente'
-})
-}catch(error){
-  console.log(error)
-  res.status(404).json({
-    mensaje:'Error el prudcto solicitado no pudo ser modificado'
-  })
-}
-}
-export const borrarProducto = async(req, res)=>{
-try{
-// buscar un producto por id y borrar
-await Producto.findByIdAndDelete(req.params.id)
-// responder al front si pude eliminarolo
-res.status(200).json({
-  mensaje:'El producto fue  correctamente eliminado'
-})
-}catch(error){
-  console.log(error)
-  res.status(404).json({
-    mensaje:'Error el prudcto solicitado no pudo ser eliminado'
-  })
-}
-}
+export const obtenerProducto = async (req, res) => {
+  try {
+    // obtener el parametro
+    console.log(req.params.id);
+    // peditle a la bd buscar el documento que concide con el id del parametro
+    const productoBuscado = await Producto.findById(req.params.id);
+    // responder con el producto encontrado
+    res.status(200).json(productoBuscado);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error no se pudo encontrar el producto solicitado",
+    });
+  }
+};
+export const editarProducto = async (req, res) => {
+  try {
+    // buscar el producto por el id, luego modificar los datos con el body
+    await Producto.findByIdAndUpdate(req.params.id, req.body);
+    // responder al frontend
+    res.status(200).json({
+      mensaje: "El producto fue editado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error el prudcto solicitado no pudo ser modificado",
+    });
+  }
+};
+export const borrarProducto = async (req, res) => {
+  try {
+    // buscar un producto por id y borrar
+    await Producto.findByIdAndDelete(req.params.id);
+    // responder al front si pude eliminarolo
+    res.status(200).json({
+      mensaje: "El producto fue  correctamente eliminado",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error el prudcto solicitado no pudo ser eliminado",
+    });
+  }
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -6,15 +6,57 @@ import {
   listarProductos,
   obtenerProducto,
 } from "../controllers/productos.controllers";
+import { check } from "express-validator";
 
 const router = Router();
 
-router.route("/productos").get(listarProductos).post(crearProducto);
+router
+  .route("/productos")
+  .get(listarProductos)
+  .post(
+    [
+      check("nombreProducto")
+        // para validar que el campo no este vacio
+        .notEmpty()
+        // mensaje por si esta vacio
+        .withMessage("El nombre de un producto es un dato obligatorio")
+        .isLength({ min: 2, max: 10 })
+        .withMessage(
+          "El nombre del producto debe tener entre 2 y 50 caracteres"
+        ),
+      check("precio")
+        .notEmpty()
+        .withMessage("El precio es un dato obligatorio")
+        .isNumeric()
+        .withMessage("el precio debe ser un numero")
+        .custom((value) => {
+          // aqui poner la logica que nos interes
+          if (value >= 1 && value <= 10000) {
+            return true;
+          } else {
+            // crear nuevo error
+            throw new Error("el Precio debe estar entre 1 y 10000");
+          }
+        }),
+      check("imagen")
+        .notEmpty()
+        .withMessage("la imagen es un dato obligatorio")
+        .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+        .withMessage("debe enviar una URL valida"),
+      check("categoria")
+        .notEmpty()
+        .withMessage("la categoria es un dato obligatorio")
+        .isIn(["bebida caliente", "bebida fria", "dulce", "salado"])
+        .withMessage("La categoria debe ser correcta"),
+    ],
+    crearProducto
+  );
 
-router.route('/productos/:id')
-.get(obtenerProducto)
-.put(editarProducto)
-.delete(borrarProducto)
+router
+  .route("/productos/:id")
+  .get(obtenerProducto)
+  .put(editarProducto)
+  .delete(borrarProducto);
 
 export default router;
 
