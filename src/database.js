c044320b64bf92722e8cd856ce05666941e0237f
matias <pagosptcmatias@gmail.com>
2022-11-03 16:51:01 -0300

    limpie codigo

--- src/database.js
+++ src/database.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 // localhost: 127.0.0.1
 // peticion a base de datos
-const url= 'mongodb://127.0.0.1:27017/cafe-branch';
+// const url= 'mongodb://127.0.0.1:27017/cafe-branch';
+const url= 'mongodb+srv://mahumada:f1qaMbagdv8Knrv8@cluster0.nkoqvy9.mongodb.net/cafe-branch-c61';
 
 const connectDB = async ()=>{
 
